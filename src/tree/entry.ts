ad1bb9a37508e12fc11048ffbf5b87e8171c864f
BuildTools <lucas.forster@lehmenkaut.de>
2019-04-08 22:24:55 +0200

    :hankey: Demonstrate TypeScript's getter notation
    Even while providing a `get` keyword, TS requires writing a verbose getter function and renaming the private (protected) variable (the code convention suggests an underscore: `_childEntries`). This looks very ugly to me, I will take a deeper look into replacing it with a pure functional reducer based scheme.

--- src/tree/entry.ts
+++ src/tree/entry.ts
@@ -5,10 +5,16 @@ export default abstract class Entry {
     abstract readonly structure: string;
     abstract readonly icon: React.ComponentType;
 
+    protected _childEntries: Entry[] = [];
+
     constructor (
         readonly id: string,
         readonly title: string,
     ) {}
+
+    get childEntries() : Entry[] {
+        return this._childEntries;
+    }
 }
 
 export class ProjectEntry extends Entry {
