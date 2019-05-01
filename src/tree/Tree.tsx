13fe6d29ea0d4192af013c77b9d40fe0e1ecb16c
BuildTools <lucas.forster@lehmenkaut.de>
2019-05-01 19:15:19 +0200

    :sparkles: Implement open/close entry actions

--- src/tree/Tree.tsx
+++ src/tree/Tree.tsx
@@ -12,22 +12,59 @@ export interface TreeState {
     childs?: TreeState[];
 }
 
-export enum TreeAction {
+export type TreeAction =
+    | TreeActionOpen
+    | TreeActionClose
+;
+enum TreeActionType {
+    Close = 'CLOSE',
+    Open = 'OPEN',
 }
+interface TreeActionProps {
+    position: number[],
+}
+interface TreeActionOpen extends TreeActionProps {
+    type: TreeActionType.Open,
+}
+interface TreeActionClose extends TreeActionProps {
+    type: TreeActionType.Close,
+}
+
 export const TreeReducer = (state: TreeState, action: TreeAction): TreeState => {
-    switch(action) {
+    // navigate
+    switch(action.type) {
+        // to position
+        case TreeActionType.Close:
+        case TreeActionType.Open:
+            if (action.position !== [] && state.childs && (action.position[0] < state.childs.length))
+                return TreeReducer(state.childs[action.position[0]], {...action, position: action.position.slice(1)});
+        // to parent
     }
+
+    // manipulate state
+    switch(action.type) {
+        case TreeActionType.Close:
+            return {...state, open: false};
+        case TreeActionType.Open:
+            return {...state, open: true};
+    }
+
     return state;
 }
 
 export interface TreeProps {
     state: TreeState,
     dispatch: React.Dispatch<TreeAction>,
-    position?: number[],
+    position: number[],
 }
 export const Tree: React.FunctionComponent<TreeProps> = (props) => (
     <List className='list' disablePadding>
-        <ListItem button id={props.state.entry.id} /* onClick setOpen */>
+        <ListItem button id={props.state.entry.id}
+            onClick={() => props.dispatch(props.state.open
+                ? { type: TreeActionType.Close, position: props.position }
+                : { type: TreeActionType.Open, position: props.position }
+            )}
+        >
             <ListItemIcon>
                 { React.createElement(EntryIcon(props.state.entry.type)) }
             </ListItemIcon>
